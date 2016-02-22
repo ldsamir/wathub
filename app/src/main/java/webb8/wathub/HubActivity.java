db4f4473025bebe6f4c41c8f359b4cc072da048e
Jeyhun Orujlu <jorujlu@uwaterloo.ca>
2016-02-21 20:43:07 -0500

    added profile support

--- app/src/main/java/webb8/wathub/HubActivity.java
+++ app/src/main/java/webb8/wathub/HubActivity.java
@@ -17,17 +17,20 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.widget.EditText;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import webb8.wathub.models.Parsable;
 import webb8.wathub.models.Post;
+import webb8.wathub.models.Profile;
 
 public class HubActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -66,7 +69,9 @@ public class HubActivity extends AppCompatActivity
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         if (position == Action.PROFILE.getId()) {
-
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, HubFragment.newInstance(position))
+                    .commit();
         }
 
         if (position == Action.MESSAGES.getId()) {
@@ -209,7 +214,27 @@ public class HubActivity extends AppCompatActivity
             View rootView = null;
 
             if (sectionNumber == Action.PROFILE.getId()) {
+                rootView = inflater.inflate(R.layout.fragment_profile, viewContainer, false);
+                ParseQuery<ParseObject> query = Profile.getQuery();
+                ParseUser user = ParseUser.getCurrentUser();
+                query.whereEqualTo("owner", user);
+                try {
+                    List<ParseObject> objects = query.find();
+                    Profile cur_user = Profile.getInstance(objects.get(0));
+                    EditText firstNameView = (EditText) rootView.findViewById(R.id.firstName);
+                    EditText lastNameView = (EditText) rootView.findViewById(R.id.lastName);
+                    EditText birthDayView = (EditText) rootView.findViewById(R.id.birthDay);
+                    EditText phoneView = (EditText) rootView.findViewById(R.id.phoneNumber);
+                    firstNameView.setText(cur_user.getFirstName());
+                    lastNameView.setText(cur_user.getLastName());
+                    SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm a", Locale.CANADA);
+                    if(cur_user.getBirthday() != null){
+                        birthDayView.setText(format.format(cur_user.getBirthday()));
+                    }
+                    phoneView.setText(cur_user.getPhone());
+                } catch (ParseException e) {
 
+                }
             }
 
             if (sectionNumber == Action.MESSAGES.getId()) {
